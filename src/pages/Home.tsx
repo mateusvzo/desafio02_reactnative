25313c4f0877019210e6913a926399715a16e33c
mateusvzo <mcarvalhoveloso@hotmail.com>
2021-08-27 13:40:42 -0300

    finally
    challange finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,39 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+interface TaskProps {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+    setTasks([...tasks, data])
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updatedTask = tasks.map( task => ({...task}));
+    const taskDone = updatedTask.find(task => task.id === id)
+    
+    if(!taskDone) {
+      return;
+    }
+
+    taskDone.done = !taskDone.done;
+    setTasks(updatedTask)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(task => task.filter(t => t.id !== id))
   }
 
   return (
